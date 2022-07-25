8dcb1b9b6e3371c39dbd4fd2260afe68d0bc79e3
Antonio Shivers <antonioshivers2500@yahoo.com>
2022-07-24 23:38:32 -0400

    added a negative number

--- models/test/transaction.js
+++ models/test/transaction.js
@@ -8,7 +8,7 @@ module.exports = [
   },
   {
     item_name: "cat food",
-    amount: 50,
+    amount: -50,
     date: "01/01/2022",
     from: "petco",
     category: "food",
@@ -19,5 +19,12 @@ module.exports = [
     date: "01/01/2022",
     from: "bank",
     category: "savings",
+  },
+  {
+    item_name: "bus fare",
+    amount: -100.69,
+    date: "05/10/3000",
+    from: "metro",
+    category: "transportation",
   },
 ];
